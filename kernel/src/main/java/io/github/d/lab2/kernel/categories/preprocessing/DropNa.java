3992eff4bd67ef4ae733e9ea90d5d4c65138732f
Chenzhou <chenzhou.liao@etu.unice.fr>
2023-01-09 10:36:02 +0100

    Fix DropNa

--- kernel/src/main/java/io/github/d/lab2/kernel/categories/preprocessing/DropNa.java
+++ kernel/src/main/java/io/github/d/lab2/kernel/categories/preprocessing/DropNa.java
@@ -1,13 +1,14 @@
 package io.github.d.lab2.kernel.categories.preprocessing;
 
 import io.github.d.lab2.kernel.generator.Visitor;
+import io.github.d.lab2.notebook.Notebook;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class DropNa extends PreprocessingElement {
 
     @Override
-    public void accept(Visitor<StringBuffer> visitor) {
+    public void accept(Visitor<Notebook> visitor) {
         visitor.visit(this);
     }
 }
